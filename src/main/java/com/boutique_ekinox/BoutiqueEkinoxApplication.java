3f082e0b87a60598cc592f320359a0028345e809
rado <hei.rado.2@gmail.com>
2023-08-24 12:36:09 +0300

    first commit

---
+++ src/main/java/com/boutique_ekinox/BoutiqueEkinoxApplication.java
@@ -0,0 +1,13 @@
+package com.boutique_ekinox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueEkinoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueEkinoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boutique_ekinox/BoutiqueEkinoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutique_ekinox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueEkinoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
