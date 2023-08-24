6ce1c474b8c5036645612ed3ee34004bb6bedc1f
rado <hei.rado.2@gmail.com>
2023-08-24 12:41:27 +0300

    feat: second commit

--- src/test/java/com/boutique_ekinox/BoutiqueEkinoxApplicationTests.java
+++ src/test/java/com/boutique_ekinox/BoutiqueEkinoxApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BoutiqueEkinoxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
