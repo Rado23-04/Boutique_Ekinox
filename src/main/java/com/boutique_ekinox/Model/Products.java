49466078c0e09aa81534d2a6fb65473d474f284a
rado <hei.rado.2@gmail.com>
2023-08-24 12:59:24 +0300

    feat: create classe client, products, reservation

---
+++ src/main/java/com/boutique_ekinox/Model/Products.java
@@ -0,0 +1,17 @@
+package com.boutique_ekinox.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Products {
+    private int id;
+    private String name;
+    private String description;
+    private float price;
+    private int quantity;
+}
---
+++ src/main/java/com/boutique_ekinox/Model/Reservation.java
@@ -0,0 +1,14 @@
+package com.boutique_ekinox.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Reservation {
+    private String reservation_date;
+}
