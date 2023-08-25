682ca793adf63967c2cab5da05529e0621a49441
rado <hei.rado.2@gmail.com>
2023-08-25 21:49:43 +0300

    fix: add service insert and selectId payment

--- src/main/java/com/boutique_ekinox/Service/BuyService.java
+++ src/main/java/com/boutique_ekinox/Service/BuyService.java
@@ -1,11 +1,14 @@
 package com.boutique_ekinox.Service;
 
 import com.boutique_ekinox.Model.Buy;
+import com.boutique_ekinox.Model.Client;
 import com.boutique_ekinox.Repository.BuyDAO;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BuyService {
     private BuyDAO buyDAO;
@@ -21,5 +24,21 @@ public class BuyService {
     }
     public void deletePayment(int id) throws SQLException {
         buyDAO.delete(id);
+    }
+    public Buy insert(Buy toInsert) {
+        try {
+            this.buyDAO.insert(toInsert);
+            return toInsert;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occurred while inserting the payment.");
+        }
+    }
+    public Optional<Buy> IdBuy (int id)throws SQLException{
+        try {
+            return buyDAO.selectById(id);
+        }catch (SQLException e){
+            throw new  RuntimeException("An error occurred while finding the payment.");
+        }
     }
 }
