2c81af504069716661ff1a25eaad3fbcc8e069d4
rado <hei.rado.2@gmail.com>
2023-08-25 21:25:29 +0300

    fix: add service delete and selectId by reservation

--- src/main/java/com/boutique_ekinox/Service/ReservationService.java
+++ src/main/java/com/boutique_ekinox/Service/ReservationService.java
@@ -5,6 +5,9 @@ import com.boutique_ekinox.Model.Reservation;
 import com.boutique_ekinox.Repository.ReservationDAO;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
 @Service
 public class ReservationService {
     private ReservationDAO reservationDAO;
@@ -18,6 +21,16 @@ public class ReservationService {
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("An error occurred while inserting the reservation.");
+        }
+    }
+    public void deletereservation(int id) throws SQLException {
+        reservationDAO.delete(id);
+    }
+    public Optional<Reservation> IdReservation (int id)throws SQLException{
+        try {
+            return reservationDAO.selectById(id);
+        }catch (SQLException e){
+            throw new  RuntimeException("An error occurred while finding the reservation.");
         }
     }
 }
