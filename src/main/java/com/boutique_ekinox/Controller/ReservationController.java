70a7035df3c954fd6ce16c5c3f8e23e461ecf635
rado <hei.rado.2@gmail.com>
2023-08-25 21:22:11 +0300

    fix: add endpoint get and delete reservation

--- src/main/java/com/boutique_ekinox/Controller/ReservationController.java
+++ src/main/java/com/boutique_ekinox/Controller/ReservationController.java
@@ -3,9 +3,10 @@ package com.boutique_ekinox.Controller;
 import com.boutique_ekinox.Model.Products;
 import com.boutique_ekinox.Model.Reservation;
 import com.boutique_ekinox.Service.ReservationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 @RestController
 public class ReservationController {
@@ -16,5 +17,14 @@ public class ReservationController {
     @PostMapping("/insert_reservation")
     public Reservation insertTodo(@RequestBody Reservation toInsert){
         return reservationService.insert(toInsert);
+    }
+    @DeleteMapping("/delete_reservation/{id}")
+    public String deleteClient(@PathVariable int id) throws SQLException {
+        reservationService.deletereservation(id);
+        return "Client successfully deleted";
+    }
+    @GetMapping("/id_reservation/{id}")
+    public Optional<Reservation> selectProduct (@PathVariable int id) throws SQLException{
+        return reservationService.IdReservation(id);
     }
 }
